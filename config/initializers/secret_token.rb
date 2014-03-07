5eb629d337aa25463d6d36607f46f10c548ba400
long-long-float <niinikazuki@yahoo.co.jp>
2014-03-08 00:02:31 +0900

    first commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MrScheduler::Application.config.secret_key_base = '8fde7425dda6127e9649a5df11f068e9d08ef4ffb093694fb38515b8ebc3ba1cda55a7252272992bdeda6af817bf1666598be0c119f291580fa86ebb9fb287b2'
