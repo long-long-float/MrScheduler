fe1028d6867ce4061b72d31c2dd0819fe9027910
long-long-float <niinikazuki@yahoo.co.jp>
2014-03-12 14:20:08 +0900

    installed acts_as_taggable_on

--- Gemfile
+++ Gemfile
@@ -45,3 +45,5 @@ end
 # gem 'debugger', group: [:development, :test]
 
 gem 'devise'
+
+gem 'acts-as-taggable-on'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140312050552) do
+ActiveRecord::Schema.define(version: 20140312051919) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -29,6 +29,24 @@ ActiveRecord::Schema.define(version: 20140312050552) do
     t.datetime "updated_at"
     t.integer  "owner",      default: 0, null: false
   end
+
+  create_table "taggings", force: true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       limit: 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+
+  create_table "tags", force: true do |t|
+    t.string "name"
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true
 
   create_table "tasks", force: true do |t|
     t.integer  "group_id"
